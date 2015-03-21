e3fcffbd64e6f9db40049ee4051b5c8221f4edb0
miquelarranz <miquelarranz@github.com>
2015-03-21 22:32:37 +0100

    Login fixed

--- H2pOints/app/routes.php
+++ H2pOints/app/routes.php
@@ -5,12 +5,12 @@ Route::get('home', [
     'uses' => 'HomeController@index'
 ]);
 
-Route::get('/', [
+Route::get('login', [
     'as' => 'login_path',
     'uses' => 'SessionsController@create'
 ]);
 
-Route::post('/', [
+Route::post('login', [
     'as' => 'login_path',
     'uses' => 'SessionsController@store'
 ]);
